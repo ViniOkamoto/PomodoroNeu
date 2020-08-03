12e32b82f09ea353c4098d60d66573ac8a7ea609
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-08-02 21:35:51 -0300

    removed status bar and bottom bar nav mobile

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoroapp/app/presentation/viewmodel/theme/theme_store.dart';
 import 'package:pomodoroapp/core/router.dart';
@@ -10,6 +11,7 @@ import 'package:provider/provider.dart';
 class PomodoroApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MultiProvider(
       providers: [
         Provider<ThemeStore>(
