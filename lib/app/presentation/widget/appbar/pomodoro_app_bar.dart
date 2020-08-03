0e0315c52249d2ec477374bdd7f5068e3ee8632f
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-08-02 21:37:36 -0300

    refact components

--- lib/app/presentation/widget/appbar/pomodoro_app_bar.dart
+++ lib/app/presentation/widget/appbar/pomodoro_app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoroapp/app/presentation/widget/appbar/header_app_bar.dart';
+import 'package:pomodoroapp/app/presentation/widget/appbar/setting_button.dart';
 
 
 class PomodoroAppBar extends StatelessWidget implements PreferredSizeWidget {
